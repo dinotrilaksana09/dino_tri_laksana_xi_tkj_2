bb3d8db9087cb77a0da1848f2a4167a15a2a45af
dinotrilaksana09 <148985216+dinotrilaksana09@users.noreply.github.com>
2023-10-25 21:20:39 +0700

    Add files via upload

---
+++ dino_praktek_2.py
@@ -0,0 +1,14 @@
+# nama : DINO TRI LAKSANA
+# kelas : XI - TKJ 2
+# nomor absen : 09
+
+#Soal : Buat program python yang mengambil estimasi waktu selesai proyek  dan tanggal target selesai. Jika estimasi waktu selesai lebih awal atau sama dengan target,  program harus mencetak "Tepat waktu" jika tidak, program harus mencetak "Terlambat".
+
+#jawaban 
+estimasi_selesai = input("waktu selesai : ")
+estimasi_target = input("waktu target : ")
+
+if estimasi_selesai <= estimasi_target:
+     print("Tepat waktu")
+else:
+    print("Terlambat")
---
+++ dino_praktek_9.py
@@ -0,0 +1,18 @@
+# nama : DINO TRI LAKSANA
+# kelas : XI - TKJ 2
+# nomor absen : 09
+
+#Buat program Python yang mengambil status persiapan proyek dan menentukan apakah proyek 
+#tersebut dapat diluncurkan. jika status  persiapan "Siap", program harus mencetak "Proyek 
+#diluncurkan". Jika status  persiapan "Tunda", program harus mencetak "Proyek ditunda"
+
+status_persiapan_proyek = str(input("Masukkan status persiapan proyek (Siap/Tunda): ").lower())
+
+if status_persiapan_proyek == "siap":
+    tindakan = "diluncurkan."
+elif status_persiapan_proyek == "tunda":
+    tindakan = "ditunda."
+else:
+    tindakan = "tidak valid."
+
+print(f"Proyek akan {tindakan}")
